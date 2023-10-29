44d7fd1dc55675821ca2577330830ce68c4df0bc
saywhite <saywhite@umich.edu>
2023-10-29 12:18:13 -0500

    start adding pybind for imGuiNodes

--- cmake/add_mini_bundle_bindings.cmake
+++ cmake/add_mini_bundle_bindings.cmake
@@ -2,8 +2,11 @@ function(add_mini_bundle_bindings)
     set(bindings_main_folder ${MINIBUNDLE_PATH}/external/bindings_generation/cpp/)
     include(${bindings_main_folder}/all_pybind_files.cmake)
 
+    set(bindings_widget_folder ${MINIBUNDLE_PATH}/widget/bindings_generation/cpp/)
+    include(${bindings_widget_folder}/all_pybind_files.cmake)
+
     #########################################################################
-    # Build python module that provides bindings to the library hello_imgui
+    # Build python module that provides bindings
     #########################################################################
     set(bound_library mini_bundle)                 # The library for which we are building bindings
     set(python_native_module_name _mini_bundle)    # This is the native python module name
---
+++ widget/ImGuiNodes/bindings/pybind_ImGuiNodes.cpp
@@ -0,0 +1,11 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
+
+#include "imgui.h"
+#include "implot/implot.h"
+#include "implot/implot_internal.h"
+
+
+namespace py = pybind11;
